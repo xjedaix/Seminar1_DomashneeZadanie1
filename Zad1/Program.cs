3045907e459eb0c7a2ce6a31a5b1d6271f6e8b4f
Evgeniy <evgeniyklimow@gmail.com>
2023-01-29 16:50:32 +0300

    add projects: zad 2, zad 3, zad 4

--- Zad1/Program.cs
+++ Zad1/Program.cs
@@ -3,3 +3,17 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.Clear(); // очищает консоль
+Console.WriteLine("Введите число 1: "); // задаем вопрос пользователю данной командой
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: "); // задаем вопрос пользователю данной командой
+int number2 = int.Parse(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine("первое число больше чем второе");
+}
+if(numberA < numberB)
+{
+    Console.WriteLine("Первое число меньше чем второе");
+}
