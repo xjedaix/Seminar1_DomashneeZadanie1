27cdbb6b52cfc598e3eca15a2fe24982e5c328fe
Evgeniy <evgeniyklimow@gmail.com>
2023-02-01 11:11:11 +0300

    добавил решение 4 задачи

--- Zad4/Program.cs
+++ Zad4/Program.cs
@@ -3,3 +3,12 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+Console.Clear();
+Console.Write ("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int A1 = 2;
+while (A1 <= num)
+{
+    Console.Write ($" {A1} ");
+    A1 = A1 + 2; 
+}
